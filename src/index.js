7632dab0468893bed39338c02d9c52c5bf1bb763
naritai <alexandr.voronetskiy@gmail.com>
2023-04-25 17:44:06 +0300

    disable devtools in production

--- src/index.js
+++ src/index.js
@@ -1,9 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { BrowserRouter } from 'react-router-dom';
-import './index.css';
 import App from './components/App/App';
 import { AuthProvider } from './context/AuthProvider';
+import { disableReactDevTools } from '@fvilers/disable-react-devtools';
+import './index.css';
+
+if (process.env.NODE_ENV === 'production') {
+  disableReactDevTools();
+}
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
